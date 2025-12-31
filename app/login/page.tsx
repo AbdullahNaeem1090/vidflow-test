70bb46742da9602d7917e31f07c2a4a4f7626150
Abdullah Naeem <masenko2023@gmail.com>
2025-12-31 22:31:22 +0500

    Lets try again

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,18 @@
 "use client"
 
 import LoginForm from "@/page-components/Login";
-import { useAuthStore } from "@/Store/authStore";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 
 export default function LoginPage() {
 
-  const {currUser}=useAuthStore()
   const router = useRouter();
 
   useEffect(()=>{
-    console.log(currUser)
-   if(currUser) router.push("/")
-  }, [currUser, router])
+    const user=localStorage.getItem("accessToken")
+   if(user) router.replace("/")
+  }, [router])
 
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
